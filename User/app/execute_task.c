7f6a6d0be196a2a6760fe1bf6a4f2972c705b106
berberman <1793913507@qq.com>
2018-07-25 12:35:05 +0800

    add statrmachine framework

--- User/app/execute_task.c
+++ User/app/execute_task.c
@@ -13,27 +13,58 @@ extern uint8_t test_servo;
 
 //uint16_t servo_angle = 1000;
 
+int current=0;
+int last=0;
 
+int _delay=0;
+
+
+#include "can_device.h"
+
+int16_t mCurrent[1];
 
 void execute_task(const void* argu)
 {
-   
-
-   while(1)
-  {
-   //测试电机控制程序
-    //test_moto_control();
+   while(1){
+		switch(current){
+		case 0:
+			mCurrent[0]=2333;
+			break;
+		case 1:
+			delay(1000);
+			break;
+		case 2:
+			mCurrent[0]=0;
+			break;
+		case 99999:
+			if(_delay--!=-1)
+			 osDelay(1);
+			else current=last;
+			break;
+		}
+		if(current<99997)
+			jump_next();
 		
-		if(rc.sw2 == 3)
-			{
-		   test_servo=1;
-			}
-   
-		else
-		  {
-			(test_servo=0);
-		  }
-			
-    osDelay(5);
+		set_test_motor_current(mCurrent);
+		osDelay(5);
   }
+}
+
+static void jump(int i){
+	last=current;
+	current=i;
+}
+
+static void delay(int i){
+	_delay=i/6.4;
+	jump(99999);
+}
+
+static void jump_back(){
+	current=last;
+	last=current;
+}
+
+static void jump_next(){
+	jump(current+1);
 }
